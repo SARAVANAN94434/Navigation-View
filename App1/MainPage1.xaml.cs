10ce66acc04a67654fb758dc27ec877da9387bc7
SARAVANAN S <35600911+SARAVANAN94434@users.noreply.github.com>
2018-01-21 00:51:13 +0530

    Add files via upload

---
+++ App1/MainPage1.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    public sealed partial class MainPage1 : Page
+    {
+        public MainPage1()
+        {
+            this.InitializeComponent();
+        }
+    }
+    
+}
---
+++ App1/VLC.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    public sealed partial class VLC : Page
+    {
+        public VLC()
+        {
+            this.InitializeComponent();
+        }
+
+        private void vlc_DoubleTapped(object sender, Windows.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
+        {
+            FullscreenToggle();
+        }
+        public bool IsFullscreen { get; set; } = false;
+        
+        private void FullscreenToggle()
+        {
+            this.IsFullscreen = !this.IsFullscreen;
+
+            if (this.IsFullscreen)
+            {
+                vlc.Width = Window.Current.Bounds.Width;
+                vlc.Height = Window.Current.Bounds.Height;                
+            }
+            else
+            {
+                vlc.Width = 640;
+                vlc.Height = 480;
+            }
+        }
+    }
+    
+}
